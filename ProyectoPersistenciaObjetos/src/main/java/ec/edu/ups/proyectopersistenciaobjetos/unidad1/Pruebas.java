485e509feef5fca7034f140667fe972120157b4e
d-armandoC <diego.cale92@gmail.com>
2024-11-19 04:24:23 -0500

    Inicio del Proyecto. COnfiguración

---
+++ ProyectoPersistenciaObjetos/src/main/java/ec/edu/ups/proyectopersistenciaobjetos/unidad1/Pruebas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.proyectopersistenciaobjetos.unidad1;
+
+/**
+ *
+ * @author 59398
+ */
+public class Pruebas {
+    
+}
