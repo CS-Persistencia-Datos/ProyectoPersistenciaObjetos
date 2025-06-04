e55558f80522c56487fa4a4b74ad9bb93a94ce72
d-armandoC <diego.cale92@gmail.com>
2025-06-03 23:35:53 -0500

    Actualizaciones.s

---
+++ ProyectoPersistenciaObjetos/src/main/java/ec/edu/ups/proyectopersistenciaobjetos/unidad4/DataProcessor.java
@@ -0,0 +1,32 @@
+
+package ec.edu.ups.proyectopersistenciaobjetos.unidad4;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+
+public class DataProcessor {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("com.tutorial_ConcurrenciaJPA_jar_1.0-SNAPSHOTPU");
+        EntityManager em = entityManagerFactory.createEntityManager();
+            try {
+            em.getTransaction().begin();
+             // Insertar un nuevo cliente
+            Cliente cliente = new Cliente();
+            cliente.setNombre("Cliente desde proceso");
+            em.persist(cliente);
+             // Consultar clientes
+            TypedQuery<Cliente> query = em.createQuery("SELECT c FROM Cliente c", Cliente.class);
+            query.getResultList().forEach(c -> System.out.println("Cliente: " + c.getNombre()));
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+            entityManagerFactory.close();
+        }
+    }
+}
